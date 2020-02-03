f44810a2cf23f1ba6c319e3029975d92f5c9e009
rkukin <romakukin@gmail.com>
2020-02-03 17:21:10 +0200

    removed PropTypes from App component

--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -6,21 +6,21 @@ const FeedbackLabel = styled.p``;
 
 export default function Statistics({ good, neutral, bad, total, positivePercentage }) {
 
-  return (
-    <div>
-      <FeedbackLabel>Good: {good}</FeedbackLabel>
-      <FeedbackLabel>Neutral: {neutral}</FeedbackLabel>
-      <FeedbackLabel>Bad: {bad}</FeedbackLabel>
-      <FeedbackLabel>Total: {total}</FeedbackLabel>
-      <FeedbackLabel>Positive feedback: {positivePercentage} %</FeedbackLabel>
-    </div>
-  )
+    return (
+        <div>
+            <FeedbackLabel>Good: {good}</FeedbackLabel>
+            <FeedbackLabel>Neutral: {neutral}</FeedbackLabel>
+            <FeedbackLabel>Bad: {bad}</FeedbackLabel>
+            <FeedbackLabel>Total: {total}</FeedbackLabel>
+            <FeedbackLabel>Positive feedback: {positivePercentage} %</FeedbackLabel>
+        </div>
+    )
 }
 
 Statistics.propTypes = {
-  good: PropTypes.number.isRequired,
-  neutral: PropTypes.number.isRequired,
-  bad: PropTypes.number.isRequired,
-  total: PropTypes.number.isRequired,
-  positivePercentage: PropTypes.number.isRequired
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    positivePercentage: PropTypes.number.isRequired
 };
